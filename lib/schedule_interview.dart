bbb5e3dba520103b290e225e55e5db81e6abea50
masihtamsoy <masihtamsoy@gmail.com>
2022-02-10 20:56:49 +0530

    collect interviewers and feed on dropdown

--- lib/schedule_interview.dart
+++ lib/schedule_interview.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import './form_builder/json_schema.dart';
 import './common/date_time_constants.dart';
+import './common/constants.dart';
+import 'package:supabase/supabase.dart' as supa;
 
 class ScheduleInterview extends StatefulWidget {
   ScheduleInterview({Key key}) : super(key: key);
@@ -30,7 +32,8 @@ class _ScheduleInterviewState extends State<ScheduleInterview> {
         {
           'name': 'interviewerEmail',
           'prefix': '',
-          'type': 'TextInput',
+          'type': 'Dropdown',
+          'options': getInterviewersOption(),
           // 'keyboardType': 'number',
           'labelText': "Interviewer Email",
           // not work
@@ -62,6 +65,44 @@ class _ScheduleInterviewState extends State<ScheduleInterview> {
     return form;
   }
 
+  List interviewerAvailable = [];
+
+  dynamic getInterviewerAvailability() async {
+    final client = supa.SupabaseClient(
+        SupaConstants.supabaseUrl, SupaConstants.supabaseKey);
+
+    final selectResponse = await client
+        .from('interviewer_availibilty')
+        .select('*')
+        .is_('availaiblity', true)
+        .execute();
+
+    String data = json.encode({});
+
+    if (selectResponse.error == null) {
+      // print('response.data: ${selectResponse.data}');
+      interviewerAvailable = selectResponse.data;
+      // data = json.encode({"jobs": selectResponse.data});
+
+      // print("<><><><><><><><><><><>< $data");
+    } else {
+      // print('>>>>>>>>>>>>>>>>>>>selectResponse.error: ${selectResponse.error}');
+      FocusScope.of(context).unfocus();
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(selectResponse.error.message)));
+    }
+
+    // return data;
+  }
+
+  List getInterviewersOption() {
+    List options = [];
+    interviewerAvailable.forEach((element) {
+      options.add(element['interviewer_email']);
+    });
+    return options;
+  }
+
   @override
   Widget build(BuildContext context) {
     /// get dates
@@ -69,16 +110,37 @@ class _ScheduleInterviewState extends State<ScheduleInterview> {
 
     print(DateTimeConstants.getTimes(context));
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Schedule Interview'),
-        ),
-        body: Container(
-          child: JsonSchema(
-              form: _createForm(context),
-              onSubmitSave: (dynamic response, _formKey) {
-                print('on submit ${_formKey.currentState.fields}');
-              }),
-        ));
+    return FutureBuilder(
+        future:
+            getInterviewerAvailability(), // function where you call your api
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: Text('Please wait its loading...'));
+          } else {
+            if (snapshot.hasError)
+              return Center(child: Text('Error: ${snapshot.error}'));
+            else {
+              return Container(
+                child: JsonSchema(
+                    form: _createForm(context),
+                    onSubmitSave: (dynamic response, _formKey) {
+                      print('on submit ${_formKey.currentState.fields}');
+                    }),
+              );
+            }
+          }
+        });
+
+    // return Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Schedule Interview'),
+    //     ),
+    //     body: Container(
+    //       child: JsonSchema(
+    //           form: _createForm(context),
+    //           onSubmitSave: (dynamic response, _formKey) {
+    //             print('on submit ${_formKey.currentState.fields}');
+    //           }),
+    //     ));
   }
 }
